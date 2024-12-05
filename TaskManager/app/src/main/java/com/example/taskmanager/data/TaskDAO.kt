bbf81727985a250d9cf15adaf54521822fe2d31b
disara7 <129250994+disara7@users.noreply.github.com>
2024-12-05 18:59:23 +0530

    entity, DAO, repo, Room DB, ViewModel
    MVVM pattern: with the ViewModel managing UI-related data and the Repository handling the logic for accessing the database.

---
+++ TaskManager/app/src/main/java/com/example/taskmanager/data/TaskDAO.kt
@@ -0,0 +1,24 @@
+package com.example.taskmanager.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): LiveData<List<Task>>
+}
---
+++ TaskManager/app/src/main/java/com/example/taskmanager/data/TaskRepository.kt
@@ -0,0 +1,11 @@
+package com.example.taskmanager.data
+
+import androidx.lifecycle.LiveData
+
+class TaskRepository(private val taskDao: TaskDao) {
+    val allTasks: LiveData<List<Task>> = taskDao.getAllTasks()
+
+    suspend fun insert(task: Task) = taskDao.insertTask(task)
+    suspend fun update(task: Task) = taskDao.updateTask(task)
+    suspend fun delete(task: Task) = taskDao.deleteTask(task)
+}
